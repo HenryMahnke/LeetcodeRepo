314fc448013f50e9dc77e22b556f432a9f057ec0
mahnk040 <mahnk040@umn.edu>
2025-06-06 23:50:58 -0500

    optimizing using array of integer representation of letters and hashing using tuple

--- day_6.py
+++ day_6.py
@@ -45,36 +45,47 @@ for i in range(0,m) //outer loop
     checking equality, and if it equals then place in array 
 """
 
-def groupAnagrams(strs): 
-    dictArray = []
-    indexArray = []
-    outputStrs = []
-    for i in range(len(strs)):
-        curDict = {}
-        for j in range(len(strs[i])):
-            curVal = curDict.get(strs[i][j],0) 
-            curDict[strs[i][j]] = curVal + 1
-        print(curDict)
-        if not dictArray:
-            dictArray.append([curDict])
-            indexArray.append([i])
-            outputStrs.append([strs[i]])
+# def groupAnagrams(strs): 
+#     dictArray = []
+#     indexArray = []
+#     outputStrs = []
+#     for i in range(len(strs)): #this runs m times 
+#         curDict = {}
+#         for j in range(len(strs[i])): #this runs n times
+#             curVal = curDict.get(strs[i][j],0) 
+#             curDict[strs[i][j]] = curVal + 1
+#         print(curDict)
+#         if not dictArray:
+#             dictArray.append([curDict])
+#             indexArray.append([i])
+#             outputStrs.append([strs[i]])
 
-        else:
-            added = False
-            for k in range(len(dictArray)): #exclusive of i
-                print("test")
-                print(dictArray[k])
-                if curDict == dictArray[k][0]:
-                    dictArray[k].append([curDict])
-                    indexArray[k].append(i) #the i'th index is what we are currently indexing through
-                    outputStrs[k].append(strs[i])
-                    added = True
-            if not added:
-                indexArray.append([i])
-                outputStrs.append([strs[i]])
-                dictArray.append([curDict])
-    return outputStrs
+#         else:
+#             added = False
+#             for k in range(len(dictArray)): #exclusive of i #this runs m-1 times
+#                 print("test")
+#                 print(dictArray[k])
+#                 if curDict == dictArray[k][0]:
+#                     dictArray[k].append([curDict])
+#                     indexArray[k].append(i) #the i'th index is what we are currently indexing through
+#                     outputStrs[k].append(strs[i])
+#                     added = True
+#             if not added:
+#                 indexArray.append([i])
+#                 outputStrs.append([strs[i]])
+#                 dictArray.append([curDict])
+#     return outputStrs
+
+#all in for time complexity we have m (n + m-1), we don't necessarily know that m-1 < n
+#so we get O(mn + m^2) but because we check equlaity in that loop which runs in O(n)
+#so we get O(mn + m^2 * c) - not great
+#as for space complexity we get O(3 * mn) = O(mn)
+
+
+
+
+
+#can assume m > n
     # print(dictArray) 
     # print(indexArray)
     # print(outputStrs)
@@ -83,9 +94,40 @@ def groupAnagrams(strs):
 
 
 # strs = ["act","tac","pots","tops","cat","stop","hat"]
-strs = [""]
+# strs = [""]
 #the output we got was indexes 0,1,4, which is act tac, cat, that is correct 
 #then we got 2,3,5 that is pots, tops, stop, that is correct, then we got 6 which is hat, that is correct
 #so now just instead of printing the index then we just want to append to the output
 #so this will use more than O(m) space.
+# print(groupAnagrams(strs))
+
+
+
+
+#now for the more optimized version, the last hint was that we could use 
+#an array of size O(26), since the characters et is a through z. and we can use this to count 
+#the frequency of each character ina  string. Then use the array as the key in the hashmap to group the strings 
+
+def groupAnagrams(strs): 
+    biggerDict = {}
+    outputStrs = []
+    for i in range(len(strs)): #this runs m times 
+        curArray = [0] * 26
+        for j in range(len(strs[i])): #this runs n times
+            # get the value that we want it to be in the array
+            index = ord(strs[i][j]) - ord('a')
+        
+            curArray[index] +=1
+        key = tuple(curArray)
+        if key not in biggerDict:
+            biggerDict[key] = [strs[i]]
+        else: 
+            biggerDict[key].append(strs[i])
+    # print(biggerDict)
+    for k in biggerDict:
+        outputStrs.append(biggerDict[k])
+    return outputStrs
+strs = ["act","tac","pots","tops","cat","stop","hat"]
+
+# strs = [""]
 print(groupAnagrams(strs))
