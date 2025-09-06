19ed13e5c44390c69b5118b05a62bff60f7a1b18
Henry Mahnke <mahnk040@umn.edu>
2025-09-06 09:31:57 -0500

    time based key val sort with bin search

--- TimeBasedKeyValSort.py
+++ TimeBasedKeyValSort.py
@@ -17,39 +17,29 @@ class TimeMap:
         print("get: key", key, "timestamp", timestamp)
         ls = self.d.get(key,None)
         if ls:
-            #then we know that there do exist some keys in it, then we need to binary search, but lets start with the brute force
-            for i in range(len(ls)):
-                print("ls at i" , ls[i], ls[i][0])
-                if ls[i][0] == timestamp: 
-                    return ls[i][1]
-            #if the key is not in it then we want to return the most recent(time) value of k 
-            #we can do this by iterating backward through the list and checking for the first timestamp that is less 
-            # for i in range(len(ls)-1, -1,-1):
-            #     if ls[i][0] < timestamp: 
-            #         #first one that is less than timestamp
-            #         return ls[i][1]
-            tripped = False
-            for i in range(len(ls)):
-                if ls[i][0] > timestamp:
-                    tripped = True
-                    print("tripped")
-                    break
-            if not tripped:
-                print("not tripped?")
-                #the last value in the list 
-                return ls[-1][1]
-            else: # if tripped 
-                print("going to return the i-1 value")
-                print(ls)
-
-                if ls[i-1] and i-1 >-1:
-                    return ls[i-1][1]
+            #if the list exists we want to binary search the list
+            #need to binary search along them 
+            l = 0 
+            r = len(ls)-1
+            while l <= r: 
+                m = (r+l)//2
+                if ls[m][0] == timestamp:
+                    return ls[m][1]
+                elif ls[m][0] <timestamp: 
+                    l = m+1
                 else: 
-                    return "" 
+                    r = m-1
+            print(ls[m], m)
+            #if they are going to be equal and it's not there 
+            #1,2,4,6 target 5 
+            #  L M R
+            if ls[l-1] and ls[l-1][0] < timestamp: 
+                return ls[l-1][1]
+            else: 
+                return ""
+
+
             
-            #if the list exists, but it is not at time stamp, and there is no time stamp that is less 
-            #then we return empty 
-            return ""
         return ""
 
 
